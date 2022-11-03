8c6b20fbd121c1a709e4cf7f178e56b6e68cbaf9
Platinum <platinumemirate@gmail.com>
2022-11-03 07:45:07 +0100

    remove bi

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,6 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
         <link rel="icon" href="/asset/mantenar.svg" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"></link>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" defer></script>
         <script src="https://js.paystack.co/v1/inline.js" defer></script>
